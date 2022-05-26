1c4928f709ab8b76f76080d2cc2942fab1168856
nico <nico@aleph.engineering>
2022-05-26 16:08:49 -0400

    #3 Create UserRepository.php

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Entities\CaoUsuario;
+use App\Repositories\_Core\Abstraction\AbstractRepository;
+use Illuminate\Support\Collection;
+use JetBrains\PhpStorm\Pure;
+
+class UserRepository extends AbstractRepository
+{
+    #[Pure]
+    public function __construct(CaoUsuario $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function getConsultants(): Collection
+    {
+        return $this->model::join('permissao_sistema', 'cao_usuario.co_usuario', '=', 'permissao_sistema.co_usuario')
+            ->where('permissao_sistema.co_sistema', 1)
+            ->where('permissao_sistema.in_ativo', 1)
+            ->whereIn('permissao_sistema.co_tipo_usuario', [0, 1, 2])
+            ->get();
+    }
+}
