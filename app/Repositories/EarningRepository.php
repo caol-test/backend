7999d4f26377eef6a21b0c730d6fb082fe18a107
nico <nico@aleph.engineering>
2022-05-29 17:24:09 -0400

    #6 Create EarningRepository.php

---
+++ app/Repositories/EarningRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Entities\VEarning;
+use App\Repositories\_Core\Abstraction\AbstractRepository;
+use Illuminate\Support\Collection;
+use JetBrains\PhpStorm\Pure;
+
+class EarningRepository extends AbstractRepository
+{
+    #[Pure]
+    public function __construct(VEarning $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function getByDateRange(array $consultants, string $from, string $to): Collection
+    {
+        return $this->model::query()
+            ->whereBetween('year_month', [$from, $to])
+            ->whereIn('user', $consultants)
+            ->get();
+    }
+}
