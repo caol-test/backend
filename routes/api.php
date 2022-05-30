ea15c2324a62d2faa6fb61597a8dbbd399311190
nico <nico@aleph.engineering>
2022-05-30 02:54:17 -0400

    #9 Define v1 prefix for current API version

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,28 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtAuth;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('jwt')
-    ->controller(JwtController::class)
+Route::prefix('v1')
     ->group(function () {
-        Route::get('/', 'renewJwt');
+        Route::prefix('jwt')
+            ->controller(JwtController::class)
+            ->group(function () {
+                Route::get('/', 'renewJwt');
+            });
+
+        Route::prefix('users')
+            ->middleware([JwtAuth::class])
+            ->controller(UserController::class)->group(function () {
+                Route::prefix('consultants')->group(function () {
+                    Route::get('/', 'getConsultants');
+                });
+            });
+
+        Route::prefix('earnings')
+            ->middleware([JwtAuth::class])
+            ->controller(EarningController::class)->group(function () {
+                Route::get('/', 'getEarnings');
+                Route::get('/fixed-cost-avg', 'getFixedCostAverage');
+                Route::get('/percent', 'getPercentsByConsultant');
+            });
     });
 
-Route::prefix('users')
-    ->middleware([JwtAuth::class])
-    ->controller(UserController::class)->group(function () {
-        Route::prefix('consultants')->group(function () {
-            Route::get('/', 'getConsultants');
-        });
-    });
-
-Route::prefix('earnings')
-    ->middleware([JwtAuth::class])
-    ->controller(EarningController::class)->group(function () {
-        Route::get('/', 'getEarnings');
-        Route::get('/fixed-cost-avg', 'getFixedCostAverage');
-        Route::get('/percent', 'getPercentsByConsultant');
-    });
